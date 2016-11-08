9969bb5cad26274b656f1060df7b0a83b70b30e4
Vyacheslav Kuzharov <marhuz.61@gmail.com>
2016-11-08 23:19:40 +0300

    Improve config.js

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ var config = require('../config/config');
 // Use native Node promises
 mongooseHandler.Promise = global.Promise;
 // connect to MongoDB
-mongooseHandler.connect('mongodb://' + config.db.host + '/' + config.db.name)
+mongooseHandler.connect(config.db.url)
     .then(function(){console.log('connection succesful')})
     .catch(function(err) {console.error(err)});
 
